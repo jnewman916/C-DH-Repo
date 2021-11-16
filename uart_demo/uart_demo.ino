98aeb7b42d3a69dbfbf0b80f00dd37e8bca183ac
John Newman <newman17@students.rowan.edu>
2021-11-16 13:59:06 -0500

    Added demo arduino code
    Simulates commands

---
+++ uart_demo/uart_demo.ino
@@ -0,0 +1,34 @@
+int LED = 8;
+int SEN = A0;
+int data = 0;
+char cmd;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+}
+
+void cmdSwitch(char cmd){
+  switch(cmd){
+    case 'a': 
+      digitalWrite(LED, LOW);
+      break;
+    case 'b': 
+      digitalWrite(LED, HIGH);
+      break;
+    case 'c':
+      data = analogRead(SEN);
+      Serial.println(data);
+      break;
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0){
+    cmd = Serial.read();
+    cmdSwitch(cmd);
+  }
+  delay(250);
+}
